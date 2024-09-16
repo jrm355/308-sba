e72d829b7293705b9c1506e1559d81bb80f9c571
jrm355 <jeremykazdan34@gmail.com>
2024-09-16 11:55:10 -0400

    organizing the info given to me

---
+++ script.js
@@ -0,0 +1,112 @@
+// The provided course information.
+const CourseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const AssignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: 
+    
+    //AssignmentInfo
+    [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const LearnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+  
+
+
+//   {
+//     // the ID of the learner for which this data has been collected
+//     "id": number,
+
+//     // the learner’s total, weighted average, in which assignments
+//     // with more points_possible should be counted for more
+//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
+//     // would have a weighted average score of 240/300 = 80%.
+//     "avg": number,
+
+//     // each assignment should have a key with its ID,
+
+//     // and the value associated with it should be the percentage that
+//     // the learner scored on the assignment (submission.score / points_possible)
+//     <assignment_id>: number,
+
+//     // if an assignment is not yet due, it should not be included in either
+//     // the average or the keyed dictionary of scores
+// }
+
+//keep in mind
+//If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error,
+//letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+//You should also account for potential errors in the data that your program receives. What if points_possible is 0?
+ //You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
+
+
+
+  
